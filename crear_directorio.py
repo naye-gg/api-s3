82e28035aa7e5a0875ff11b8274b5dd15f7a3f18
Nayeli Belén Guerrero Gutierrez <nayeli.guerrero@utec.edu.pe>
2025-11-06 21:39:11 -0500

    Add files via upload

--- crear_directorio.py
+++ crear_directorio.py
@@ -3,17 +3,19 @@ import json
 
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
-    body = json.loads(event['body'])
+
+    body = event['body'] if isinstance(event['body'], dict) else json.loads(event['body'])
     bucket = body['bucket']
     directorio = body['directorio']
-
 
     try:
         key = f"{directorio.strip('/')}/"
         s3.put_object(Bucket=bucket, Key=key)
         return {
             'statusCode': 200,
-            'body': json.dumps({'mensaje': f'Directorio "{directorio}" creado en bucket "{bucket}".'})
+            'body': json.dumps({
+                'mensaje': f'Directorio "{directorio}" creado en bucket "{bucket}".'
+            })
         }
     except Exception as e:
         return {
